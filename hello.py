23c1fa3b2d583fb853f83b4e3dedde7da5202bf8
HassanMunene <awanzihassan@gmail.com>
2023-05-12 17:47:52 +0300

    I have added a config object that creates a connection to a sqlite database to enable database operations with the application using sqlalchemy

--- hello.py
+++ hello.py
@@ -8,9 +8,15 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
