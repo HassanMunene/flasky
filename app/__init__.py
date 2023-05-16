57fadff53772a93d10133a27bf3d419f67bd1403
HassanMunene <awanzihassan@gmail.com>
2023-05-16 12:22:57 +0300

    I have removed the mail extension configuration of the app because i already removed the mail configurations

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,6 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
-    mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     from .main import main as main_blueprint
