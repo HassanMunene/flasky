f4e44ceae05b329a84ed4424973089cdaa4d4376
HassanMunene <awanzihassan@gmail.com>
2023-05-16 12:56:04 +0300

    correcting some typo error

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class NameForm(FlaskForm):
     """
     form class definition
     """
-    name = StringField('What is your name?'. validators=[DataRequired()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
