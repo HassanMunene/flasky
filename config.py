e0e9c5b81545df99800eb09ee1dca0910ffe582e
HassanMunene <awanzihassan@gmail.com>
2023-05-16 11:11:58 +0300

    I have removed the email configurations this is because I want to have as few confugurations so that I can actually how the new structure works and how do forms templates and database connects

--- config.py
+++ config.py
@@ -13,12 +13,6 @@ class Config:
     This is the base class
     """
     SECTRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS =os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'devtools14347@gmail.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -48,9 +42,9 @@ class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 config = {
-        'development': DevelopmentConfig
-        'testing': TestingConfig
-        'production': ProductionConfig
+        'development': DevelopmentConfig,
+        'testing': TestingConfig,
+        'production': ProductionConfig,
         'default': DevelopmentConfig
         }
 
