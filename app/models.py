73df2268521e399cba650ee89aea8d6a729f59b9
HassanMunene <awanzihassan@gmail.com>
2023-05-17 11:03:23 +0300

    I have added werkzeug module that will handle the generation and verification of password hashes, this password hashes will also be stored in a new column named password_hash

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Role(db.Model):
     """
@@ -26,6 +27,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         """
