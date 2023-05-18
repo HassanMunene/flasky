01bbac652670e7b4f2f52ec02cb307e12939f0f6
HassanMunene <awanzihassan@gmail.com>
2023-05-18 09:47:37 +0300

    I have added the implementation of logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,14 +6,14 @@ for now /login route is added
 """
 from flask import render_template, redirect, url_for, request, flash
 from . import auth
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from ..models import User
 from .forms import LoginForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    logform = LoginForm()
-    if from.validate_on_submit():
+    form = LoginForm()
+    if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
@@ -22,4 +22,11 @@ def login():
                 next = url_for('main.index')
             return redirect(next)
         flash('Invalid username or password')
-    return render_template('auth/login.html', logform=logform)
+    return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
