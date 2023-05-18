e93762ea573c997ecb9e7ed381ff87762a9d4e4a
HassanMunene <awanzihassan@gmail.com>
2023-05-18 15:15:27 +0300

    I have generated another migration script that adds two more fields to the User model namely email field and password field

---
+++ app/auth/forms.py
@@ -0,0 +1,15 @@
+"""
+The class defines a form that will
+be rendered for logging in
+it will have the email, password, remember me and submit area
+"""
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
