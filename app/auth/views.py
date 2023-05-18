0c17927a1355729dc9fbc9b54c920b7c7b34a787
HassanMunene <awanzihassan@gmail.com>
2023-05-18 09:15:57 +0300

    I ve updated the view function that handles the login route so that it can process logins by users and prevent unauthorised access

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,9 +4,22 @@ the routes associated with authentication using its
 routes decorator
 for now /login route is added
 """
-from flask import render_template
+from flask import render_template, redirect, url_for, request, flash
 from . import auth
+from flask_login import login_user
+from ..models import User
+from .forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth/login.html')
+    logform = LoginForm()
+    if from.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid username or password')
+    return render_template('auth/login.html', logform=logform)
