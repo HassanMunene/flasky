7ea118e35213470d4ecd72bcb494a36ac07b48d5
HassanMunene <awanzihassan@gmail.com>
2023-05-19 11:16:37 +0300

    fixing some syntax error

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,7 +8,7 @@ from flask import render_template, redirect, url_for, request, flash
 from . import auth
 from flask_login import login_user, logout_user, login_required
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,11 +33,11 @@ def logout():
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
+    form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('You can now login')
         return redirect(url_for('auth.login'))
-    return render_template('auth/register.html')
+    return render_template('auth/register.html', form=form)
