73e0cf3b2e29cbac34a508157e5dc46b8d350266
HassanMunene <awanzihassan@gmail.com>
2023-05-20 11:56:54 +0300

    I have added a route and a view function that handles the link that is clicked after the confirmation email is sent

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,9 +6,10 @@ for now /login route is added
 """
 from flask import render_template, redirect, url_for, request, flash
 from . import auth
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from ..models import User
 from .forms import LoginForm, RegistrationForm
+from .. import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -41,3 +42,15 @@ def register():
         flash('You can now login')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
