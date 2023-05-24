aeec589efb1a6274ba5536bd6b882b00ae659e9a
HassanMunene <awanzihassan@gmail.com>
2023-05-24 11:26:47 +0300

    added methods that will help manage permissions within the Role model

--- app/models.py
+++ app/models.py
@@ -23,6 +23,20 @@ class Role(db.Model):
         if self.permissions is None:
             self.permissions = 0
 
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permission(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
     def __repr__(self):
         """
         returns string rep of the object
