e69ebb42a3958e1a3615b835006a019486690514
HassanMunene <awanzihassan@gmail.com>
2023-05-25 17:21:32 +0300

    a form for regular user to fill in or her information

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,3 +8,14 @@ class NameForm(FlaskForm):
     """
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class EditProfileForm(FlaskForm):
+    """
+    a form that regular user will use
+    to fill in his/her information to be
+    displayed on the profile page
+    """
+    name = StringField('Real Name', validators=[Length(1, 64)])
+    location = StringField('Location', validators=[Length(1, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit)
