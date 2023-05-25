0575017875382279b637c9a7a50342b48e0e6c97
HassanMunene <awanzihassan@gmail.com>
2023-05-25 11:50:09 +0300

    we added a few more fields to the user model for use in user profiles

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from flask import current_app
+from datetime import datetime
 
 class Role(db.Model):
     """
@@ -87,6 +88,11 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def can(self, perm):
         return self.role is not None and self.role.has_permission(perm)
