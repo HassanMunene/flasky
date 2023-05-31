a37ac94513878de7743b9c14842cc9a3f3d8ef3c
HassanMunene <awanzihassan@gmail.com>
2023-05-31 16:34:27 +0300

    we've set up the setting for the development work only

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
-import os
 """
+We have created a configuration class that sets up a
+number of settings we need to get the application running
+such as connecting to the database,
+setting up the email, creating a secret key
 get the absolute path for the directory containing
 this file: basedir
 """
+import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    SECRET_KEY = "you cannot guess my key yoh"
     FLASKY_ADMIN = "awanzihassan@outlook.com"
     FLASKY_MODERATOR = "sultanhamud081@outlook.com"
     MAIL_SERVER = "smtp.office356.com"
@@ -15,3 +21,11 @@ class Config:
     MAIL_USERNAME = "devtools14347@outlook.com"
     MAIL_PASSWORD = "Munene14347"
     MAIL_USE_TLS = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.join(basedir, 'dev_data.sqlite')
+    FLASK_DEBUG = True
+
+    @static
+    def init_app(app):
+        pass
+
