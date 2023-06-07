ee68ff69f6af1b20e983d70131247ae48fd1e667
HassanMunene <awanzihassan@gmail.com>
2023-06-07 11:28:36 +0300

    Flask httpauth initialization

--- app/api/errors.py
+++ app/api/errors.py
@@ -9,3 +9,11 @@ def page_not_found(e):
     return render_template('404.html').404
 
 
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        reponse = jsonify({'error': 'internal server error'})
+        response.status_coe = 500
+        return response
+    return render_template('500.html').500
+
